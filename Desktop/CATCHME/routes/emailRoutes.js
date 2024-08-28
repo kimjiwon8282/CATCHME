d75104e30ae21cfe42a70eb5066c614c83bdeb77
kimjiwon <jiwonkim512@gmail.com>
2024-08-28 15:41:03 +0900

    폴더 삭제 후 변경 사항 반영

--- Desktop/CATCHME/routes/emailRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { sendVerificationCode, verifyCode } = require('../controllers/emailController');
-const router = express.Router();
-
-router.route('/send-verification-code').post(sendVerificationCode);
-router.route('/verify-code').post(verifyCode);
-
-module.exports = router;
