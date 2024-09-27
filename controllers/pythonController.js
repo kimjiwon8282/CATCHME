10278481cb8f32480af21bbcefb863b7430782c4
nyeong-deo <131740017+nyeong-deo@users.noreply.github.com>
2024-09-27 21:36:51 +0900

    role에따라 결과 조회

--- controllers/pythonController.js
+++ controllers/pythonController.js
@@ -23,15 +23,32 @@ const createResult = asyncHandler(async (req, res) => {
 // 전체 결과 보기
 const getAllResults = asyncHandler(async (req, res) => {
     const userId = req.session.userId;
+    let searchUserId;
 
-    // userId에 해당하는 모든 결과를 검색
-    const results = await Result.find({ userId });
+    if (req.session.role === 'USER') {
+        // USER인 경우: userId에 해당하는 모든 결과를 검색
+        searchUserId = userId;
+    } else if (req.session.role === 'GUARDIAN') {
+        // GUARDIAN인 경우: contactId에 해당하는 모든 결과를 검색
+        const guardian = await User.findById(userId);
+        
+        if (!guardian || !guardian.contactId) {
+            return res.status(404).json({ message: '보호자 정보를 찾을 수 없습니다.' });
+        }
+
+        searchUserId = guardian.contactId;
+    } else {
+        return res.status(403).json({ message: '권한이 없습니다.' });
+    }
+
+    //searchUserId에 해당하는 모든 결과를 검색
+    const results = await Result.find({ userId: searchUserId });
 
     if (results.length === 0) {
         return res.status(404).json({ message: '결과를 찾을 수 없습니다.' });
     }
 
-    res.status(200).json(results);
+    return res.status(200).json(results);
 });
 
 // @desc Get the latest result by userId
@@ -39,8 +56,23 @@ const getAllResults = asyncHandler(async (req, res) => {
 // 최근 결과 하나 보기
 const getResult = asyncHandler(async (req, res) => {
     const userId = req.session.userId;
+    let searchUserId;
 
-    const result = await Result.findOne({ userId }).sort({ createdAt: -1 }); // 최신 결과 검색
+    if (req.session.role === 'USER') {
+        // USER인 경우: userId에 해당하는 모든 결과를 검색
+        searchUserId = userId;
+    } else if (req.session.role === 'GUARDIAN') {
+        // GUARDIAN인 경우: contactId에 해당하는 모든 결과를 검색
+        const guardian = await User.findById(userId);
+        if (!guardian || !guardian.contactId) {
+            return res.status(404).json({ message: '보호자 정보를 찾을 수 없습니다.' });
+        }
+        searchUserId = guardian.contactId;
+    } else {
+        return res.status(403).json({ message: '권한이 없습니다.' });
+    }
+
+    const result = await Result.findOne({ userId:searchUserId }).sort({ createdAt: -1 }); // 최신 결과 검색
 
     if (!result) {
         return res.status(404).json({ message: '결과를 찾을 수 없습니다.' });
